fca9f00081eaba8742eb50af527c8e891c2b10ba
tajjacob <taj.jacob@gmail.com>
2024-07-31 17:03:39 +0800

    ♻️ chore: List of Chapter Reciters api failed connection

---
+++ lib/src/features/audio/data/repositories/chapter_reciter_repo.dart
@@ -0,0 +1,7 @@
+abstract class ChapterReciterRepoInterface {
+  factory ChapterReciterRepoInterface() => ChapterReciterRepo();
+}
+
+class ChapterReciterRepo implements ChapterReciterRepoInterface {
+  // the connection failed for this api (/resources/chapter_reciters)
+}
